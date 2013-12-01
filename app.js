9e5c78b226b0399db162ef02dcec5fe3cd3c9d5f
Chris Oei <code@oei.io>
2013-12-01 12:36:30 -0800

    Add status codes and DELETE functionality

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ app.use(express.bodyParser());
 var dataStore = {};
 
 app.get('/media/:ns/:id', function(req, res) {
-    dataStore[req.params.ns] &&
-        res.send(dataStore[req.params.ns][req.params.id]);
+    res.send(200, dataStore[req.params.ns] &&
+             dataStore[req.params.ns][req.params.id]);
 });
 
 app.put('/media/:ns/:id', function(req, res) {
     dataStore[req.params.ns] = dataStore[req.params.ns] || {};
     dataStore[req.params.ns][req.params.id] = req.body;
-    res.send();
+    res.send(202);
+});
+
+app.delete('/media/:ns/:id', function(req, res) {
+    dataStore[req.params.ns] &&
+        delete dataStore[req.params.ns][req.params.id];
+    res.send(204);
 });
 
 app.listen(63446);
