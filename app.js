2825e974c2fe69465f5458c52823d8440bec60a6
Chris Oei <code@oei.io>
2013-12-02 13:02:49 -0800

    :  Allow CORS M  app.js

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ function saveDataStore() {
     }, 3000);
 }
 
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
 app.get('/db/:ns/:id', function(req, res) {
     res.send(200, dataStore[req.params.ns] &&
              dataStore[req.params.ns][req.params.id]);
