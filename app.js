71fcb2784e397c14d0329c9e4d813880cf50baa5
Chris Oei <code@oei.io>
2013-12-03 11:56:44 -0800

    :  Add Access-Control-Allow-Methods header for CORS M  app.js

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ function saveDataStore() {
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT, DELETE');
     next();
 });
 
