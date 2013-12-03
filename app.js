40013bf9d2c58778f74b1a653377c797244d0f37
Chris Oei <code@oei.io>
2013-12-03 11:11:22 -0800

    :  Formatting fix M  app.js

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ function saveDataStore() {
 }
 
 app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
 });
 
 app.get('/db/:ns/:id', function(req, res) {
