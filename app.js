ade3ead3f9820da53789a91da02cef0ebf62cd24
Chris Oei <code@oei.io>
2013-12-06 21:34:59 +0000

    :  Add Content-Type to allowed headers M  app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function saveDataStore() {
 
 app.all('/*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     res.header('Access-Control-Allow-Methods', 'PUT, DELETE, OPTIONS');
     next();
 });
